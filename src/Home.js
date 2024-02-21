9a55a7d08954d46346c4c00b5e67f73cf1a6247b
K1-aD <153174317+K1-aD@users.noreply.github.com>
2024-02-22 05:01:00 +0800

    Added Core File

---
+++ src/Home.js
@@ -0,0 +1,29 @@
+import {useState} from 'react';
+
+const Home = () => {
+//let name='mario'
+const[name, setName]=useState('mario');
+const[age, setAge]=useState(25);
+
+const [blogs, setBlogs] = useState([
+    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+  ])
+
+    return (
+        <div className="Home">
+             <div className="home">
+      {blogs.map(blog => (
+        <div className="blog-preview" key={blog.id} >
+          <h2>{ blog.title }</h2>
+          <p>Written by { blog.author }</p>
+        </div>
+      ))}
+    </div>
+        </div> 
+    );
+}
+ 
+export default Home;
+
